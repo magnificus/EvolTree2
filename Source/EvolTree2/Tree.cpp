8a9ced8cd15c875ed9ece19758bea92a17e37f0d
Tobias <tobiaselinder@gmail.com>
2018-04-11 15:38:06 +0200

    additions and improvements

--- Source/EvolTree2/Tree.cpp
+++ Source/EvolTree2/Tree.cpp
@@ -97,22 +97,22 @@ void ATree::InterpretChar(TCHAR In) {
 	}
 	}
 
-	if (ShowDebug) {
-			DrawDebugSphere(
-				GetWorld(),
-				Turtle.GetLocation(),
-				24,
-				32,
-				FColor(255, 0, 0),
-				true,
-				5.0f
-			);
-	}
+	//if (ShowDebug) {
+	//		DrawDebugSphere(
+	//			GetWorld(),
+	//			Turtle.GetLocation(),
+	//			24,
+	//			32,
+	//			FColor(255, 0, 0),
+	//			true,
+	//			5.0f
+	//		);
+	//}
 }
 
 
 void ATree::Build(FString &In) {
-	Turtle = GetActorTransform();
+	Turtle = FTransform();
 	CurrentBranch = new Branch();
 	CurrentBranch->Points.Add(Turtle);
 	Branches.Add(CurrentBranch);
@@ -128,44 +128,53 @@ void ATree::Build(FString &In) {
 			continue;
 		}
 
-		if (AvoidClipping) {
-			FCollisionQueryParams RV_TraceParams = FCollisionQueryParams(FName(TEXT("RV_Trace")), true);
-			//RV_TraceParams.bTraceComplex = true;
-			//RV_TraceParams.bTraceAsyncScene = true;
-			//RV_TraceParams.bReturnPhysicalMaterial = false;
-
-			//Re-initialize hit info
-			FHitResult RV_Hit(ForceInit);
-
-			GetWorld()->LineTraceSingleByChannel(
-				RV_Hit,        //result
-				B->Points[0].GetLocation(),    //start
-				B->Points[B->Points.Num() - 1].GetLocation(), //end
-				ECC_Visibility, //collision channel
-				RV_TraceParams
-			);
-
-			//UE_LOG(LogTemp, Display, TEXT("Line trace from %s to %s "), *(B->Points[0].ToString()), *(B->Points[B->Points.Num() - 1].ToString()));
-			if (RV_Hit.Component != nullptr) {
-				UE_LOG(LogTemp, Display, TEXT("Collision"));
-				InvalidBranches.Add(B);
-				continue;
-			}
-		}
-
-
 		TArray<FVector> VectorPoints;
 		for (FTransform t : B->Points) {
 			VectorPoints.Add(t.GetLocation());
 		}
-		Spline->SetSplinePoints(VectorPoints, ESplineCoordinateSpace::Local);
+		Spline->SetSplinePoints(VectorPoints, ESplineCoordinateSpace::World);
+
+
 
 		float Start = B->WidthStart;
 		for (int i = 0; i < Spline->GetNumberOfSplinePoints() - 1; i++) {
+			if (AvoidClipping) {
+				FCollisionQueryParams RV_TraceParams = FCollisionQueryParams(FName(TEXT("RV_Trace")), true);
+
+				FHitResult RV_Hit(ForceInit);
+
+				FVector StartL = GetActorLocation() + GetActorRotation().RotateVector(VectorPoints[i]);    //start
+				FVector EndL = GetActorLocation() + GetActorRotation().RotateVector(VectorPoints[i+1]);
+				FVector Diff = (EndL - StartL);
+				//StartL -= Diff;
+				//EndL += Diff;
+				GetWorld()->LineTraceSingleByChannel(
+					RV_Hit,        //result
+					StartL,
+					EndL, //end
+					ECC_Visibility, //collision channel
+					RV_TraceParams
+				);
+				if (ShowDebug) {
+					DrawDebugLine(
+						GetWorld(),
+						StartL,
+						EndL,
+						FColor::Red,
+						true, 5, 0, 5
+					);
+				}
+				//UE_LOG(LogTemp, Display, TEXT("Line trace from %s to %s "), *(B->Points[0].ToString()), *(B->Points[B->Points.Num() - 1].ToString()));
+				if (RV_Hit.bBlockingHit) {
+					UE_LOG(LogTemp, Display, TEXT("Collision"));
+					InvalidBranches.Add(B);
+					break;
+				}
+			}
 			FVector Loc1, Loc2;
 			FVector Tan1, Tan2;
-			Spline->GetLocationAndTangentAtSplinePoint(i, Loc1, Tan1, ESplineCoordinateSpace::Local);
-			Spline->GetLocationAndTangentAtSplinePoint(i+1, Loc2, Tan2, ESplineCoordinateSpace::Local);
+			Spline->GetLocationAndTangentAtSplinePoint(i, Loc1, Tan1, ESplineCoordinateSpace::World);
+			Spline->GetLocationAndTangentAtSplinePoint(i+1, Loc2, Tan2, ESplineCoordinateSpace::World);
 			USplineMeshComponent *S = NewObject<USplineMeshComponent>(this);
 			S->SetMobility(EComponentMobility::Type::Static);
 			S->CreationMethod = EComponentCreationMethod::UserConstructionScript;
@@ -173,7 +182,7 @@ void ATree::Build(FString &In) {
 			S->SetForwardAxis(ESplineMeshAxis::Z);
 			S->SetStartAndEnd(Loc1, Tan1, Loc2, Tan2);
 			S->SetupAttachment(RootComponent);
-			S->SetCollisionResponseToChannel(ECollisionChannel::ECC_MAX, ECollisionResponse::ECR_Overlap);
+			//S->SetCollision
 
 			float S1 = FMath::Pow(WidthMP, i) * Start;
 			float S2 = FMath::Pow(WidthMP, i+1) * Start;
@@ -181,9 +190,13 @@ void ATree::Build(FString &In) {
 			S->SetStartScale(FVector2D(S1,S1));
 			S->SetEndScale(FVector2D(S2, S2));
 			S->RegisterComponent();
+			S->SetCollisionProfileName("BlockAll");
+			S->MarkRenderStateDirty();
+			//S->SetCollisionResponseToChannel(ECollisionChannel::ECC_Visibility, ECollisionResponse::ECR_Block);
 
 		}
 	}
+	Spline->UpdateSpline();
 }
 
 void ATree::Evolve() {
